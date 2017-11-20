bd8ff824b5fe88a977fc26a2d74b0ffca00896d2
LeMa <kopojiq@gmail.com>
2017-11-20 12:55:38 +0300

    Hello from AlexGreenvi!

--- contacts/index.php
+++ contacts/index.php
@@ -3,6 +3,21 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Контакты");
 ?>
 
-Text here....
+<div class="page__contact">
+    <div class="">
+        <div class="container">
+            <div class="page__contact__text">
+                <p>
+                    ООО «Воды РЫЧАЛ-СУ» в Москве
+                </p>
+                <p>Телефон: <a href="tel:+78003334410" title="">+7 (800) 333-4410</a></p>
+                <p>Email: <a href="mailto:info@rychal-su.ru" title="">info@rychal-su.ru</a></p>
+            </div>
+            <div class="page__contact__img">
+                <img src="/assets/images/page__contact/water.png" alt="">
+            </div>
+        </div>
+    </div>
+</div>
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
