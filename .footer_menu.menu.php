a19ba9b2578374e20560023e1928384512b42eea
SultanMashukov <mashukov.s@lema.agency>
2017-12-20 16:44:06 +0300

    Added new type of menu "footer_menu".

---
+++ .footer_menu.menu.php
@@ -0,0 +1,32 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"О воде", 
+		"/water/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"О компании", 
+		"/about/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Продукция", 
+		"/production/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Контакты", 
+		"/contacnts/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
--- local/templates/main/components/bitrix/menu/bottom/template.php
+++ local/templates/main/components/bitrix/menu/bottom/template.php
@@ -1,7 +1,7 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
 
 <? if (!empty($arResult)): ?>
-    <div class="footer-item-menu hidden-xs col-sm-5 col-md-5 col-lg-5">
+    <div class="hidden-xs col-sm-5 col-md-5 col-lg-5">
         <ul class="footer-item-list">
 
             <?
