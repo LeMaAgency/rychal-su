6513cd10c1f8332e191d67629d6c2a7915870061
SultanMashukov <mashukov.s@lema.agency>
2017-12-20 16:34:26 +0300

    Added footer menu

--- local/templates/main/footer.php
+++ local/templates/main/footer.php
@@ -50,6 +50,22 @@
                     "MENU_CACHE_TIME" => "3600",
                     "MENU_CACHE_TYPE" => "N",
                     "MENU_CACHE_USE_GROUPS" => "Y",
+                    "ROOT_MENU_TYPE" => "top_left",
+                    "USE_EXT" => "N"
+                )
+            ); ?>
+            <? $APPLICATION->IncludeComponent(
+                "bitrix:menu",
+                "bottom",
+                Array(
+                    "ALLOW_MULTI_SELECT" => "N",
+                    "CHILD_MENU_TYPE" => "left",
+                    "DELAY" => "N",
+                    "MAX_LEVEL" => "1",
+                    "MENU_CACHE_GET_VARS" => array(0 => "",),
+                    "MENU_CACHE_TIME" => "3600",
+                    "MENU_CACHE_TYPE" => "N",
+                    "MENU_CACHE_USE_GROUPS" => "Y",
                     "ROOT_MENU_TYPE" => "top_right",
                     "USE_EXT" => "N"
                 )
